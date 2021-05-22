538feea0beb722b6e48291c1441869a9999985a0
rengaraj <sakthirengaraj@gmail.com>
2021-05-22 13:20:17 +0530

    Initial files for presentation added

---
+++ blind-spot-robot/slides/code/send_traffic.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if (( $# < 2 ))
+then
+    echo "no of arguments less than 2"
+    echo "Usage:./send_traffic.sh <url> <count>"
+    exit 1
+elif (( $# > 2 ))
+then
+      echo "Usage:./send_traffic.sh <url> <count>
+fi
+
+if (( count == 0 ))
+then
+
+fi	
